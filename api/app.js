f9a6ea8dc1c924cbb74c04f59b3d6d64b6e95ac5
abhishek <abhishek@gmail.com>
2024-04-06 17:26:01 +0530

    Add route files

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
+
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
   console.log("Server is running!");
