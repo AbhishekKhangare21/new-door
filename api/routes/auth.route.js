484663b3d45ae0621e5e04b9354c14a7a05351b6
abhishek <abhishek@gmail.com>
2024-04-06 18:45:05 +0530

    Add prisma

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,17 +1,10 @@
 import express from "express";
+import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  console.log("router works!");
-});
-
-router.get("/test", (req, res) => {
-  console.log("router works!");
-});
-
-router.get("/test", (req, res) => {
-  console.log("router works!");
-});
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
