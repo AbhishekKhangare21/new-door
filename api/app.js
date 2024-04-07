41123501faccb61777010c0a74f8725e6ef3382c
abhishek <abhishek@gmail.com>
2024-04-07 18:19:59 +0530

    Route completed

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
+
+app.use("/api/post", postRoute);
+
+app.use("/api/test", (req, res) => {
+  res.send("It works!");
+});
 
 app.listen(8800, () => {
   console.log("Server is running!");
